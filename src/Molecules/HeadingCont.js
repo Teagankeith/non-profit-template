5dea1efb3d9e0f3b8ee66af98a87750d983a95e2
teagankeith <teagan-keith@lambdastudents.com>
2021-08-21 13:51:30 -0400

    Fixed scaling of heading image

--- src/Molecules/HeadingCont.js
+++ src/Molecules/HeadingCont.js
@@ -4,6 +4,8 @@ const HeadingCont = styled.div`
   background: url("https://hosting.renderforestsites.com/5699226/444828/media/b2463b86049eb1625b8bc71b59540e51.jpg"); 
   width: 100%;
   height: 100vh;
+
+  background-size: cover;
 `;
 
 export default HeadingCont;
